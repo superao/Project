5da8052fd67f7f6d2d3d338fc71683f43e5d5bae
superao <18220772840@163.com>
2020-11-10 20:45:50 +0800

    将服务器程序写为了精灵进程

--- Main.cpp
+++ Main.cpp
@@ -1,48 +1,60 @@
 #include <iostream>
-#include <unistd.h>
-#include <sys/stat.h>
+#include <string>
 #include <sys/types.h>
+#include <sys/stat.h>
+#include "Tcpsocket.hpp"
+#include "Epoll.hpp"
+#include "Threadpool.hpp"
+#include "Http.hpp"
+#include "Service.hpp"
+
 using namespace std;
 
 int main()
 {
-  // 创建子进程，父进程退出
-  pid_t pid = fork();
-  if(pid < 0)
-  {
-    cout << "fork error" << endl;
-    return -1;
-  }
-  else if(pid != 0)
-  {
+    // 创建子进程，父进程退出
+    pid_t pid = fork();
+    if(pid < 0)
+    {
+        cout << "Main(): fork error!" << endl;
+        return -1;
+    }
+    else if(pid != 0)
+    {
+        // 父进程直接退出
+        return 0;
+    }
+
+    // 子进程创建新的会话
+    setsid();
+
+    // 改变当前工作路径
+    int ret = chdir("/home/superao/testgit/GitHub/Project");
+    if(ret < 0)
+    {
+        cout << "Main(): chdir error" << endl;
+        return -1;
+    }
+
+    // 设置文件掩码
+    umask(0002);
+
+    // 关闭继承下来的文件描述符
+    close(0);
+    close(1);
+    close(2);
+
+    // 核心任务
+    while(1)
+    {
+        Service srv;
+        int ret = srv.ServiceStart(8030);
+        if(ret == false)
+        {
+            cout << "main(): ServiceStart error!" << endl;
+            return -1;
+        }
+    }
+
     return 0;
-  }
-
-  // 子进程创建新的会话
-  setsid();
-
-  // 改变当前工作路径
-  int ret = chdir("/home/superao/testgit/GitHub/Project");
-  if(ret < 0)
-  {
-    cout << "chdir error" << endl;
-    return -1;
-  }
-
-  // 设置文件掩码
-  umask(0002);
-
-  // 关闭继承下来的文件描述符
-  close(0);
-  close(1);
-  close(2);
-
-  // 核心任务
-  while(1)
-  {
-
-
-  }
-
-  return 0;
 }
